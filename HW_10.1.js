58f03a840a1e02edda32d02d7b49714542c99f97
v.synytskyi <Vladyslavsynytskyi1019@gmail.com>
2025-12-18 17:00:46 +0200

    Initial Commit

---
+++ HW_10.1.js
@@ -0,0 +1,34 @@
+var arr = [
+  {
+    "userName":"Test",
+    "lastName":"Test",
+    "email":"test.test@gmail.com"
+  },
+  {
+    "userName":"Dmitro",
+    "lastName":"Porohov",
+    "email":"<dmitro.porohov@yahoo.com>"
+  },
+  {
+    "userName":"Andrii",
+    "lastName":"",
+    "email":"andrii@mail.ru"
+  }
+];
+
+var reTrusted = /^[a-z0-9]+(\.[a-z0-9]+)?@(gmail\.com|yahoo\.com)$/i;
+var reGarbage = /[<>\s]/g;
+
+var trustedEmails = [];
+
+for (var i = 0; i < arr.length; i++) {
+  var email = arr[i].email;;
+  // 1) чистим мусор
+  email = email.replace(reGarbage, "");
+
+  // 2) проверяем по правилам доверия
+ if (reTrusted.test(email)) {
+    trustedEmails.push(email);
+ }
+}
+console.log(trustedEmails);
